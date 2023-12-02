ca53a4e707a09e85bf8703b03c14cbd3fe8c9449
hmooios <135945583+hmooios@users.noreply.github.com>
2023-12-02 21:50:07 +0630

    Complete Note.swift

--- MyNotes/Note.swift
+++ MyNotes/Note.swift
@@ -8,10 +8,13 @@
 import Foundation
 
 struct Note:Codable,Identifiable{
-    var id=UUID()
-    var date:Date
-    var title:String
-    var content:String
+        var id = UUID()
+        var title: String
+        var content: String
+        var date: Date
+        var category: String
     
-    static let example = Note(date: Date(), title: "Unknown title", content: "Unknown content")
+    static let example = Note(title:"Unknown title", content: "Unknown content", date: Date(),category: "Unknown category")
 }
+
+
